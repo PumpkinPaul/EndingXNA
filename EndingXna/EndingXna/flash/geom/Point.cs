f9fbcb7c40179d3bfefc28389eee14d5dd57f61b
paulc <paul.cunningham@pumpkin-games.net>
2013-07-28 21:10:43 +0100

    New consts for Point.Zero and Point.One

--- EndingXna/EndingXna/flash/geom/Point.cs
+++ EndingXna/EndingXna/flash/geom/Point.cs
@@ -3,6 +3,9 @@
 namespace flash.geom
 {
     public class Point {
+        public static readonly Point Zero = new Point(0, 0);
+        public static readonly Point One = new Point(1, 1);
+
         public Number x;
         public Number y;
 
